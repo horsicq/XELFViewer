4c3ff1d766512ab25633d01028488d4fa47e0b79
horsicq <horsicq@gmail.com>
2021-02-26 10:05:53 +0100

    Fix

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ ELF file viewer/editor for Windows, Linux and MacOS.
 How to build on Linux
 =======
 
-Install Qt 5.12.8: https://github.com/horsicq/build_tools
+Install Qt 5.15.2: https://github.com/horsicq/build_tools
 
 Clone project: git clone --recursive https://github.com/horsicq/XELFViewer.git
 
@@ -26,7 +26,7 @@ Run build_lin64.bat
 How to build on OSX
 =======
 
-Install Qt 5.12.8: https://github.com/horsicq/build_tools
+Install Qt 5.15.2: https://github.com/horsicq/build_tools
 
 Clone project: git clone --recursive https://github.com/horsicq/XELFViewer.git
 
@@ -52,14 +52,16 @@ Run build_winxp.bat
 How to build on Windows(7-10)
 =======
 
-Install Visual Studio 2017: https://github.com/horsicq/build_tools
+Install Visual Studio 2019: https://github.com/horsicq/build_tools
 
-Install Qt 5.12.8 for VS2017: https://github.com/horsicq/build_tools
+Install Qt 5.15.2 for VS2017: https://github.com/horsicq/build_tools
 
 Install 7-Zip: https://github.com/horsicq/build_tools
 
 Clone project: git clone --recursive https://github.com/horsicq/XELFViewer.git
 
 Edit build_win32.bat ( check VS_PATH,  SEVENZIP_PATH, QT_PATH variables)
+Edit build_win64.bat ( check VS_PATH,  SEVENZIP_PATH, QT_PATH variables)
 
 Run build_win32.bat
+Run build_win64.bat
--- build_winxp.bat
+++ build_winxp.bat
@@ -1,11 +1,10 @@
 set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
 set SEVENZIP_PATH="C:\Program Files\7-Zip"
-set QT_PATH="C:\Qt5.6.3\5.6.3\msvc2013"
+set QT_PATH=C:\Qt5.6.3\5.6.3\msvc2013
 
 set BUILD_NAME=xelfviewer_winxp_portable
 set SOURCE_PATH=%~dp0
 mkdir %SOURCE_PATH%\build
-mkdir %SOURCE_PATH%\build\loader
 mkdir %SOURCE_PATH%\release
 set /p RELEASE_VERSION=<%SOURCE_PATH%\release_version.txt
 
@@ -16,8 +15,12 @@ set GUIEXE=xelfviewer.exe
 set ZIP_NAME=%BUILD_NAME%_%RELEASE_VERSION%
 set RES_FILE=rsrc
 
-cd gui_source
-%QT_PATH%\bin\qmake.exe gui_source.pro -r -spec %QT_SPEC% "CONFIG+=release"
+del %SOURCE_PATH%\XArchive\.qmake.stash
+del %SOURCE_PATH%\build_libs\.qmake.stash
+del %SOURCE_PATH%\gui_source\.qmake.stash
+
+cd build_libs
+%QT_PATH%\bin\qmake.exe build_libs.pro -r -spec %QT_SPEC% "CONFIG+=release"
 
 nmake Makefile.Release clean
 nmake
@@ -27,31 +30,48 @@ del Makefile.Debug
 
 cd ..
 
-cd winloader_source
+cd gui_source
+%QT_PATH%\bin\qmake.exe gui_source.pro -r -spec %QT_SPEC% "CONFIG+=release"
+%QT_PATH%\bin\lupdate.exe gui_source_tr.pro
 
-cl.exe /c winloader.cpp /D_USING_V110_SDK71_ /GS- /Oi-
-Rc.exe /v %RES_FILE%.rc
-link.exe winloader.obj %RES_FILE%.res user32.lib kernel32.lib shell32.lib /NODEFAULTLIB /SAFESEH:NO /SUBSYSTEM:WINDOWS,5.01 /ENTRY:entry /OUT:%SOURCE_PATH%\build\loader\%GUIEXE%
-del /s winloader.obj
-del /s %RES_FILE%.res
+nmake Makefile.Release clean
+nmake
+del Makefile
+del Makefile.Release
+del Makefile.Debug
 
 cd ..
 
-mkdir %SOURCE_PATH%\release\%BUILD_NAME%
-mkdir %SOURCE_PATH%\release\%BUILD_NAME%\base
-mkdir %SOURCE_PATH%\release\%BUILD_NAME%\base\platforms
+mkdir %SOURCE_PATH%\release\%BUILD_NAME%\
+mkdir %SOURCE_PATH%\release\%BUILD_NAME%\lang
+mkdir %SOURCE_PATH%\release\%BUILD_NAME%\platforms
 
-copy %SOURCE_PATH%\build\loader\%GUIEXE% %SOURCE_PATH%\release\%BUILD_NAME%\
-copy %SOURCE_PATH%\build\release\%GUIEXE% %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5Widgets.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5Gui.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5Core.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5OpenGL.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5Svg.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\plugins\platforms\qwindows.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\platforms\
+copy %SOURCE_PATH%\build\release\%GUIEXE% %SOURCE_PATH%\release\%BUILD_NAME%\
+copy %QT_PATH%\bin\Qt5Widgets.dll %SOURCE_PATH%\release\%BUILD_NAME%\
+copy %QT_PATH%\bin\Qt5Gui.dll %SOURCE_PATH%\release\%BUILD_NAME%\
+copy %QT_PATH%\bin\Qt5Core.dll %SOURCE_PATH%\release\%BUILD_NAME%\
+copy %QT_PATH%\bin\Qt5OpenGL.dll %SOURCE_PATH%\release\%BUILD_NAME%\
+copy %QT_PATH%\bin\Qt5Svg.dll %SOURCE_PATH%\release\%BUILD_NAME%\
+copy %QT_PATH%\plugins\platforms\qwindows.dll %SOURCE_PATH%\release\%BUILD_NAME%\platforms\
 
-copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
+copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll %SOURCE_PATH%\release\%BUILD_NAME%\
+copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll %SOURCE_PATH%\release\%BUILD_NAME%\
+
+xcopy %SOURCE_PATH%\XStyles\qss %SOURCE_PATH%\release\%BUILD_NAME%\qss /E /I
+
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_it.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_it.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_fr.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_fr.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_he.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_he.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_tr.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_tr.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_ko.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_ko.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_es.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_es.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_pt_PR.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_pt_PR.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_ja.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_ja.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_pl.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_pl.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_ru.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_ru.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_vi.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_vi.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_zh.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_zh.qm
+%QT_PATH%\bin\lrelease.exe %SOURCE_PATH%\gui_source\translation\xelfviewer_zh_TW.ts -qm %SOURCE_PATH%\release\%BUILD_NAME%\lang\xelfviewer_zh_TW.qm
 
 cd %SOURCE_PATH%\release
 if exist %ZIP_NAME%.zip del %ZIP_NAME%.zip
