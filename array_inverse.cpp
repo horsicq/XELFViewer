45fe91bdf967892c96ea27bae1765144d2fc6f4f
sarthak1320 <93342805+sarthak1320@users.noreply.github.com>
2021-10-30 19:21:57 +0530

    array_inverse.cpp
    THIS PROGRAM IS USED TO INVERSE THE ARRAY

---
+++ array_inverse.cpp
@@ -0,0 +1,46 @@
+// Iterative C++ program to reverse an array
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to reverse arr[] from start to end*/
+void rvereseArray(int arr[], int start, int end)
+{
+	while (start < end)
+	{
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+	}
+}	
+
+/* Utility function to print an array */
+void printArray(int arr[], int size)
+{
+for (int i = 0; i < size; i++)
+cout << arr[i] << " ";
+
+cout << endl;
+}
+
+/* Driver function to test above functions */
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5, 6};
+	
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	// To print original array
+	printArray(arr, n);
+	
+	// Function calling
+	rvereseArray(arr, 0, n-1);
+	
+	cout << "Reversed array is" << endl;
+	
+	// To print the Reversed array
+	printArray(arr, n);
+	
+	return 0;
+}
