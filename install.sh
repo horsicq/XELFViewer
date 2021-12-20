5e43402f69f135cef1f654d6213fc7130e46cb17
horsicq <horsicq@gmail.com>
2021-12-20 18:18:40 +0100

    Fix: 2021-12-20

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 export X_SOURCE_PATH=$PWD
 
 cp -f $X_SOURCE_PATH/build/release/xelfviewer                       /usr/bin/
-cp -f $X_SOURCE_PATH/DEBIAN/xelfviewer.desktop                      /usr/share/applications/
-cp -Rf $X_SOURCE_PATH/DEBIAN/hicolor/                               /usr/share/icons/
+cp -f $X_SOURCE_PATH/LINUX/xelfviewer.desktop                      /usr/share/applications/
+cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                               /usr/share/icons/
 cp -Rf $X_SOURCE_PATH/XStyles/qss/                                  /usr/lib/xelfviewer/
 mkdir -p  /usr/lib/xelfviewer/signatures
 cp -f $X_SOURCE_PATH/signatures/crypto.db                           /usr/lib/xelfviewer/signatures/
