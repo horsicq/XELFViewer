8b7236339e0a6d8df5e182ba59e7e0d8c0048b3f
horsicq <horsicq@gmail.com>
2021-12-28 20:25:54 +0100

    Fix: 2021-12-28

--- install.sh
+++ install.sh
@@ -7,9 +7,14 @@ else
     X_PREFIX="$1"
 fi
 
+mkdir -p  $X_PREFIX/lib/bin
+mkdir -p  $X_PREFIX/lib/share/applications
+mkdir -p  $X_PREFIX/lib/share/icons
+mkdir -p  $X_PREFIX/lib/xelfviewer
+mkdir -p  $X_PREFIX/lib/xelfviewer/signatures
+
 cp -f $X_SOURCE_PATH/build/release/xelfviewer                       $X_PREFIX/bin/
 cp -f $X_SOURCE_PATH/LINUX/xelfviewer.desktop                       $X_PREFIX/share/applications/
 cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                                $X_PREFIX/share/icons/
 cp -Rf $X_SOURCE_PATH/XStyles/qss/                                  $X_PREFIX/lib/xelfviewer/
-mkdir -p  $X_PREFIX/lib/xelfviewer/signatures
 cp -f $X_SOURCE_PATH/signatures/crypto.db                           $X_PREFIX/lib/xelfviewer/signatures/
