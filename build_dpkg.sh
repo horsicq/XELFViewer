24b6fb289a2c99462699139aa7acf8c9aab8acee
horsicq <horsicq@gmail.com>
2022-01-07 17:32:30 +0100

    Fix: 2022-01-07

--- build_dpkg.sh
+++ build_dpkg.sh
@@ -22,9 +22,15 @@ if [ -z "$X_ERROR" ]; then
         
         export X_PACKAGENAME='xelfviewer'
         export X_MAINTAINER='hors <horsicq@gmail.com>'
-        export X_DEPENDS='qt5-default, libqt5core5a, libqt5svg5, libqt5gui5, libqt5widgets5, libqt5opengl5, libqt5dbus5'
+        
         export X_HOMEPAGE='http://ntinfo.biz'
         export X_DESCRIPTION='XELFViewer is a ELF file viewer/editor.'
+        
+        if [ "$X_DEBIAN_VERSION" -ge "11" ]; then
+            export X_DEPENDS='libqt5core5a, libqt5svg5, libqt5gui5, libqt5widgets5, libqt5opengl5, libqt5dbus5'
+        else
+            export X_DEPENDS='qt5-default, libqt5core5a, libqt5svg5, libqt5gui5, libqt5widgets5, libqt5opengl5, libqt5dbus5'
+        fi
         
         create_deb_control $X_SOURCE_PATH/release/$X_BUILD_NAME/DEBIAN/control
         
