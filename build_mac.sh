74f0f2c568dbe007bd86892754ec119f59de5c2e
horsicq <horsicq@gmail.com>
2022-05-22 12:46:36 +0200

    Fix: 2022-05-22

--- build_mac.sh
+++ build_mac.sh
@@ -16,17 +16,17 @@ if [ -z "$X_ERROR" ]; then
     make_translate "gui_source_tr.pro" xelfviewer
     cd "$X_SOURCE_PATH"
 
-    check_file "$X_SOURCE_PATH/build/release/xelfviewer.app/Contents/MacOS/xelfviewer"
+    check_file "$X_SOURCE_PATH/build/release/XELFViewer.app/Contents/MacOS/XELFViewer"
     if [ -z "$X_ERROR" ]; then
-        cp -R "$X_SOURCE_PATH/build/release/xelfviewer.app"    "$X_SOURCE_PATH/release/$X_BUILD_NAME"
+        cp -R "$X_SOURCE_PATH/build/release/XELFViewer.app"    "$X_SOURCE_PATH/release/$X_BUILD_NAME"
 
-        mkdir -p $X_SOURCE_PATH/release/$X_BUILD_NAME/xelfviewer.app/Contents/Resources/signatures
-        cp -R $X_SOURCE_PATH/signatures/crypto.db       $X_SOURCE_PATH/release/$X_BUILD_NAME/xelfviewer.app/Contents/Resources/signatures
-        cp -Rf $X_SOURCE_PATH/XStyles/qss               $X_SOURCE_PATH/release/$X_BUILD_NAME/xelfviewer.app/Contents/Resources/
+        mkdir -p $X_SOURCE_PATH/release/$X_BUILD_NAME/XELFViewer.app/Contents/Resources/signatures
+        cp -R $X_SOURCE_PATH/signatures/crypto.db       $X_SOURCE_PATH/release/$X_BUILD_NAME/XELFViewer.app/Contents/Resources/signatures
+        cp -Rf $X_SOURCE_PATH/XStyles/qss               $X_SOURCE_PATH/release/$X_BUILD_NAME/XELFViewer.app/Contents/Resources/
 
-        fiximport "$X_SOURCE_PATH/build/release/xelfviewer.app/Contents/MacOS/xelfviewer"
+        fiximport "$X_SOURCE_PATH/build/release/XELFViewer.app/Contents/MacOS/XELFViewer"
 
-        deploy_qt xelfviewer
+        deploy_qt XELFViewer
 
         make_release
         make_clear
