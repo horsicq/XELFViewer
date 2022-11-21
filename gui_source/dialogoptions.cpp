50c2284bb07794173bcc933067bfb0f910d0a274
horsicq <horsicq@gmail.com>
2022-11-22 00:03:52 +0100

    Fix: 2022-11-22

--- gui_source/dialogoptions.cpp
+++ gui_source/dialogoptions.cpp
@@ -22,8 +22,7 @@
 
 #include "ui_dialogoptions.h"
 
-DialogOptions::DialogOptions(QWidget *pParent, XOptions *pOptions)
-    : QDialog(pParent), ui(new Ui::DialogOptions)
+DialogOptions::DialogOptions(QWidget *pParent, XOptions *pOptions) : QDialog(pParent), ui(new Ui::DialogOptions)
 {
     ui->setupUi(this);
 
